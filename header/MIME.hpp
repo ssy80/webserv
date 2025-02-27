e396064d9c0b9d159125f95fa77b8e1bbd2ca57d
YS <ys@workPC>
2025-02-27 22:36:03 +0800

    static class for getting MIME type using the class thru autocomplete func of ide

---
+++ header/MIME.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#ifndef __MIME_HPP
+#define __MIME_HPP
+
+#include "AServer.hpp"
+
+class MIME{
+	private:
+		MIME();
+	
+	public:
+		~MIME();
+		static const string KEY;
+		static const string HTML;
+		static const string CSS;
+		static const string GIF;
+		static const string JPEG;
+		static const string JS;
+		static const string ATOMXML;
+		static const string RSSXML;
+		static const string MATHML;
+		static const string TXT;
+		// image
+		static const string AVIF;
+		static const string PNG;
+		static const string SVGXML;
+		static const string TIFF;
+		static const string WEBP;
+		static const string ICO;
+		static const string JNG;
+		static const string BMP;
+		// application
+		static const string JSON;
+		static const string PDF;
+		static const string RTF;
+		static const string DOC;
+		static const string JAR;
+		static const string Z7;
+		static const string RAR;
+		static const string ZIP;
+		static const string BIN;
+		// audio
+		static const string MIDI;
+		static const string MPEG;
+		static const string OGG;
+		static const string M4A;
+		static const string REALAUDIO;
+		// video
+		static const string GPP;
+		static const string MP2T;
+		static const string MP4;
+		static const string MPEGV;
+		static const string QUICKTIME;
+		static const string WEBM;
+		static const string FLV;
+		static const string M4V;
+		static const string MNG;
+		static const string ASF;
+		static const string WMV;
+		static const string AVI;
+		
+
+
+
+};
+
+
+#endif
---
+++ server/MIME.cpp
@@ -0,0 +1,53 @@
+#include "../header/MIME.hpp"
+
+MIME::~MIME()
+{};
+
+const string MIME::KEY = "Content-Type: ";
+const string MIME::HTML = "text/html";
+const string MIME::CSS = "text/css";
+const string MIME::GIF = "image/gif";
+const string MIME::JPEG = "image/jpeg";
+const string MIME::JS = "application/javascript";
+const string MIME::ATOMXML = "application/atom+xml";
+const string MIME::RSSXML = "application/rss+xml";
+const string MIME::MATHML = "text/mathml";
+const string MIME::TXT = "text/plain";
+// image
+const string MIME::AVIF = "image/avif";
+const string MIME::PNG = "image/png";
+const string MIME::SVGXML = "image/svg+xml";
+const string MIME::TIFF = "image/tiff";
+const string MIME::WEBP = "image/webp";
+const string MIME::ICO = "image/x-icon";
+const string MIME::JNG = "image/x-jng";
+const string MIME::BMP = "image/x-ms-bmp";
+// Application
+const string MIME::JSON = "application/json";
+const string MIME::PDF = "application/pdf";
+const string MIME::RTF = "application/rtf";
+const string MIME::DOC = "application/msword";
+const string MIME::JAR = "application/java-archive";
+const string MIME::Z7 = "application/x-7z-compressed";
+const string MIME::RAR = "application/x-rar-compressed";
+const string MIME::ZIP = "application/zip";
+const string MIME::BIN = "application/octet-stream";
+// audio
+const string MIME::MIDI = "audio/midi";
+const string MIME::MPEG = "audio/mpeg";
+const string MIME::OGG = "audio/ogg";
+const string MIME::M4A = "audio/x-m4a";
+const string MIME::REALAUDIO = "audio/x-realaudio";
+// video
+const string MIME::GPP = "video/3gpp";
+const string MIME::MP2T = "video/mp2t";
+const string MIME::MP4 = "video/mp4";
+const string MIME::MPEGV = "video/mpeg";
+const string MIME::QUICKTIME = "video/quicktime";
+const string MIME::WEBM = "video/webm";
+const string MIME::FLV = "video/x-flv";
+const string MIME::M4V = "video/x-m4v";
+const string MIME::MNG = "video/x-mng";
+const string MIME::ASF = "video/x-ms-asf";
+const string MIME::WMV = "video/x-ms-wmv";
+const string MIME::AVI = "video/x-msvideo";
