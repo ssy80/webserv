2056d4b565f565da2555781518f2711f4348d5e6
daryl <daryloye@gmail.com>
2025-03-03 21:28:53 +0800

    rewrite getHandler

--- www/cgi-bin/image.py
+++ www/cgi-bin/image.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 import sys
 
-image_path = "./www/images/rainbow.png"
+# Path to the image
+image_path = "./www/images/42.png"
 
-with open(image_path, "rb") as f:
-    img_data = f.read()
+try:
+    # Read image as binary
+    with open(image_path, "rb") as f:
+        img_data = f.read()
 
-sys.stdout.buffer.write(img_data)
+    print("Content-Type: image/png")
+    print("Content-Length:", len(img_data))
+    print("Status: 200 OK")
+    print()
+
+    sys.stdout.buffer.write(img_data)
+
+except FileNotFoundError:
+    print("Status: 404 Not Found")
+    print("Content-Type: text/plain")
+    print()
+    print("Error: Image not found.")
