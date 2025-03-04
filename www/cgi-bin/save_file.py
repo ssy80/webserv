34e220918a12bcb14aa4b7abf17b6f960043a916
daryl <daryloye@gmail.com>
2025-03-04 17:44:32 +0800

    working on file upload

---
+++ www/cgi-bin/save_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import cgi, sys, os
+
+form = cgi.FieldStorage()
+
+#if "filename" not in form or not form["filename"].filename:
+    #print("<h2>Error: No file uploaded</h2>")
+    #sys.exit(0)
+
+fileitem = form["filename"]
+
+fn = os.path.basename(fileitem.filename)
+cwd = os.getcwd()
+save_path = os.path.join(cwd, fn)
+
+try:
+	with open(save_path, "wb") as f:
+		f.write(fileitem.file.read())
+
+	print("<h2>Success!</h2>")
+	print(f'<p>The file "{fn}" was uploaded successfully.</p>')
+
+except Exception as e:
+    print(f'<h2>Error</h2><p>Could not save file: {e}</p>')
