8d4322b147b86b91f6a531d97157b01b8295f24e
YS <yoong@c1r3s1.42singapore.sg>
2025-03-05 20:29:38 +0800

    change requestparser to get file name instead of content disposition

--- request/RequestParser.cpp
+++ request/RequestParser.cpp
@@ -50,10 +50,17 @@ void RequestParser::parsePartFormData(istringstream& stream, Request& request, c
         while (line[pos] != '"')
           name+=line.at(pos++);
         // parsing contentdisposition
-        pos = line.find("Content-Disposition: ");
+        // pos = line.find("Content-Disposition: ");
+        // if (pos != string::npos){
+        //   pos+= 21;
+        //   while (line[pos] != ';')
+        //     contentDisposition+=line[pos++];
+        // }
+        // parsing filename into formfield
+        pos = line.find("filename=");
         if (pos != string::npos){
-          pos+= 21;
-          while (line[pos] != ';')
+          pos+= 10;
+          while (line[pos] != '"')
             contentDisposition+=line[pos++];
         }
       }
